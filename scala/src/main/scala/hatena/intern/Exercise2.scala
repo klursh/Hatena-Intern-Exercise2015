2711c802e1f3e6894303afde8adea088a87e3da0
Yada <ydt.hran2@gmail.com>
2016-02-07 03:31:25 +0900

    Scala-2 1行をパースする関数を切り出す

--- scala/src/main/scala/hatena/intern/Exercise2.scala
+++ scala/src/main/scala/hatena/intern/Exercise2.scala
@@ -5,21 +5,23 @@ import scala.io.Source
 object LtsvParser {
   def parse(filePath: String): Iterable[Log] = {
     val source = Source.fromFile(filePath)
-    source.getLines().map((line: String) => {
-      val map: Map[String, String] = line.split("\t").map((item: String) => {
-        val kv = item.split(":", 2)
-        (kv(0), kv(1))
-      }).toMap
+    source.getLines().map(this.parseLine).toIterable
+  }
 
-      Log(
-        host = map("host"),
-        user = if (map("user") == "-") None else Some(map("user")),
-        epoch = map("epoch").toInt,
-        req = map("req"),
-        status = map("status").toInt,
-        size = map("size").toInt,
-        referer = if (map("referer") == "-") None else Some(map("referer"))
-      )
-    }).toIterable
+  private def parseLine(line: String): Log = {
+    val map: Map[String, String] = line.split("\t").map((item: String) => {
+      val kv = item.split(":", 2)
+      (kv(0), kv(1))
+    }).toMap
+
+    Log(
+      host = map("host"),
+      user = if (map("user") == "-") None else Some(map("user")),
+      epoch = map("epoch").toInt,
+      req = map("req"),
+      status = map("status").toInt,
+      size = map("size").toInt,
+      referer = if (map("referer") == "-") None else Some(map("referer"))
+    )
   }
 }
