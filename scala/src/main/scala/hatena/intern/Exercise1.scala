d866e0944679c7ebf604ed97854c75f1da1c6f93
Yada <ydt.hran2@gmail.com>
2016-02-07 00:12:21 +0900

    Scala-1
    Log クラスを実装

--- scala/src/main/scala/hatena/intern/Exercise1.scala
+++ scala/src/main/scala/hatena/intern/Exercise1.scala
@@ -1,9 +1,17 @@
 package hatena.intern
 
+import org.joda.time.{ DateTimeZone, DateTime }
+
 case class Log(host: String, user: String, epoch: Int, req: String, status: Int, size: Int, referer: String) {
-  def method: String = ???
-  def path: String = ???
-  def protocol: String = ???
-  def uri: String = ???
-  def time: String = ???
+  def method: String = req.split(" ")(0)
+
+  def path: String = req.split(" ")(1)
+
+  def protocol: String = req.split(" ")(2)
+
+  def uri: String = "http://" + host + this.path
+
+  val dt: DateTime = new DateTime(epoch.toLong * 1000, DateTimeZone.UTC)
+
+  def time: String = dt.toString("yyyy-MM-dd'T'HH:mm:ss")
 }
