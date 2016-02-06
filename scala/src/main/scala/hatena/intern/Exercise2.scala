6f6be2c6e7fcc440d8ecb4d544b7dad3610d6f87
Yada <ydt.hran2@gmail.com>
2016-02-07 01:43:09 +0900

    Scala-2 実装
    きたない気がする

--- scala/src/main/scala/hatena/intern/Exercise2.scala
+++ scala/src/main/scala/hatena/intern/Exercise2.scala
@@ -1,7 +1,25 @@
 package hatena.intern
 
-import scalax.file.Path
+import scala.io.Source
 
 object LtsvParser {
-  def parse(filePath: String): Iterable[Log] = ???
+  def parse(filePath: String): Iterable[Log] = {
+    val source = Source.fromFile(filePath)
+    source.getLines().map((line: String) => {
+      val map: Map[String, String] = line.split("\t").map((item: String) => {
+        val kv = item.split(":", 2)
+        (kv(0), kv(1))
+      }).toMap
+
+      Log(
+        host = map.getOrElse("host", ""),
+        user = map.getOrElse("user", ""),
+        epoch = map.getOrElse("epoch", "0").toInt,
+        req = map.getOrElse("req", ""),
+        status = map.getOrElse("status", "0").toInt,
+        size = map.getOrElse("size", "0").toInt,
+        referer = map.getOrElse("referer", "")
+      )
+    }).toIterable
+  }
 }
