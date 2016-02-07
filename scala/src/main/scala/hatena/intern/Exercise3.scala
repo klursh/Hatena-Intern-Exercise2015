a71bc69d3b961d4ce264cb0a795523a3101169ba
Yada <ydt.hran2@gmail.com>
2016-02-07 16:08:08 +0900

    Scala-3 Iterable の groupBy 関数を使う
    そういうのもあるのか

--- scala/src/main/scala/hatena/intern/Exercise3.scala
+++ scala/src/main/scala/hatena/intern/Exercise3.scala
@@ -9,15 +9,6 @@ case class LogCounter(logs: Iterable[Log]) {
 
   type User = String
   def groupByUser: Map[User, Iterable[Log]] = {
-    val map = mutable.Map[User, Seq[Log]]()
-    logs.foreach((log: Log) => {
-      val user = log.user.getOrElse("guest")
-      if (map.isDefinedAt(user)) {
-        map(user) = map(user) :+ log
-      } else {
-        map(user) = Seq(log)
-      }
-    })
-    map.toMap
+    logs.groupBy(_.user.getOrElse("guest"))
   }
 }
